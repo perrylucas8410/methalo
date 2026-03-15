57d16e1f929014b84c60075f92663ead57a62c74
perrylucas8410 <129779480+perrylucas8410@users.noreply.github.com>
2026-03-15 15:44:47 -0500

    Update injector.js

--- engine/rewrite/injector.js
+++ engine/rewrite/injector.js
@@ -1,1 +1,10 @@
+import fs from "fs";
+import path from "path";
 
+const shimPath = path.resolve("rewrite/jsShim.js");
+const shimCode = fs.readFileSync(shimPath, "utf8");
+
+export function injectShim(html) {
+  const injection = `<script>${shimCode}</script>`;
+  return html.replace("</head>", injection + "</head>");
+}
