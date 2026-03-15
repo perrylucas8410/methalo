2614b2726357739c00078333620ae2f80fb5ab8e
perrylucas8410 <129779480+perrylucas8410@users.noreply.github.com>
2026-03-15 15:16:54 -0500

    Update httpClient.js

--- engine/core/httpClient.js
+++ engine/core/httpClient.js
@@ -1,1 +1,37 @@
+import { request } from "undici";
+import { getSession } from "./sessionStore.js";
+import { filterResponseHeaders } from "./headerUtils.js";
 
+export default async function httpClient(req, res, url) {
+  const session = getSession(req, res);
+
+  const headers = {
+    "User-Agent": req.headers["user-agent"] || "",
+    "Accept": req.headers["accept"] || "*/*",
+    "Accept-Language": req.headers["accept-language"] || "",
+  };
+
+  if (session.cookies.length) {
+    headers["Cookie"] = session.cookies.join("; ");
+  }
+
+  if (req.headers["range"]) {
+    headers["Range"] = req.headers["range"];
+  }
+
+  const upstream = await request(url, {
+    method: "GET",
+    headers,
+  });
+
+  const setCookie = upstream.headers["set-cookie"];
+  if (setCookie) {
+    const arr = Array.isArray(setCookie) ? setCookie : [setCookie];
+    session.cookies = arr;
+  }
+
+  res.status(upstream.statusCode);
+  filterResponseHeaders(upstream.headers, res);
+
+  upstream.body.pipe(res);
+}
