4fa2fdb435799805b78e9a29ba5194afd0f2f909
perrylucas8410 <129779480+perrylucas8410@users.noreply.github.com>
2026-03-15 15:17:11 -0500

    Update SessionStore.js

--- engine/core/SessionStore.js
+++ engine/core/SessionStore.js
@@ -1,1 +1,25 @@
+import cookie from "cookie";
+import crypto from "crypto";
 
+const sessions = new Map();
+
+export function getSession(req, res) {
+  const cookies = cookie.parse(req.headers.cookie || "");
+  let sid = cookies.sid;
+
+  if (!sid || !sessions.has(sid)) {
+    sid = crypto.randomBytes(16).toString("hex");
+    sessions.set(sid, { cookies: [] });
+
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("sid", sid, {
+        httpOnly: true,
+        sameSite: "Lax",
+        path: "/",
+      })
+    );
+  }
+
+  return sessions.get(sid);
+}
