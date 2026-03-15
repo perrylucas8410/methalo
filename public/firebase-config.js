5a2b6078ec0c9f50f254e6dc509e381b2afe24a8
perrylucas8410 <129779480+perrylucas8410@users.noreply.github.com>
2026-03-15 01:04:45 -0500

    Create firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyBytm097ApDIFNoYBwloK03s1UZ8WaiqqM",
+  authDomain: "methalo-browser.firebaseapp.com",
+  projectId: "methalo-browser",
+  storageBucket: "methalo-browser.firebasestorage.app",
+  messagingSenderId: "901027603441",
+  appId: "1:901027603441:web:c8690c37bebab8ac4f5a50",
+  measurementId: "G-RX0N9RS0HE"
+};
