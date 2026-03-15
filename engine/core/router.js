52cd2460965176246724873e2ca2dc806a564a3b
perrylucas8410 <129779480+perrylucas8410@users.noreply.github.com>
2026-03-15 15:16:37 -0500

    Update router.js

--- engine/core/router.js
+++ engine/core/router.js
@@ -1,1 +1,19 @@
+import express from "express";
+import httpClient from "./httpClient.js";
+import { normalizeUrl } from "./urlUtils.js";
 
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const target = req.query.url;
+    if (!target) return res.status(400).send("Missing url");
+
+    const url = normalizeUrl(target);
+    await httpClient(req, res, url);
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default router;
