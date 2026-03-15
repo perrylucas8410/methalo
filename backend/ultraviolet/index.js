a6cfe6beb0cbac5c838dbfc49b13aa8cc9383546
perrylucas8410 <129779480+perrylucas8410@users.noreply.github.com>
2026-03-15 04:13:44 -0500

    Update index.js

--- backend/ultraviolet/index.js
+++ backend/ultraviolet/index.js
@@ -1,1 +1,20 @@
+import express from "express";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { createBareServer } from "@titaniumnetwork-dev/ultraviolet/bare";
+import { uvMiddleware } from "@titaniumnetwork-dev/ultraviolet/middleware";
 
+const app = express();
+const bare = createBareServer("/bare/", {
+  logErrors: true
+});
+
+app.use(express.static(uvPath));
+app.use(uvMiddleware);
+
+const server = app.listen(8080, () => {
+  console.log("Ultraviolet running on port 8080");
+});
+
+server.on("upgrade", (req, socket, head) => {
+  bare.handleUpgrade(req, socket, head);
+});
